f95f6d332a37d095c7c77ab8fffd1d6a56c61074
Zhang Xiaowei <weixiao08@qq.com>
2015-07-20 22:57:52 +0800

    增加测试视图控制器基类

---
+++ XWPageViewController/BaseViewController.h
@@ -0,0 +1,14 @@
+//
+//  BaseViewController.h
+//  XWPageViewController
+//
+//  Created by Zhang Xiaowei on 15/7/20.
+//  Copyright (c) 2015年 Zhang Xiaowei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "XWPageViewController.h"
+
+@interface BaseViewController : UIViewController<XWPageViewControllerDelegate>
+
+@end
---
+++ XWPageViewController/BaseViewController.m
@@ -0,0 +1,72 @@
+//
+//  BaseViewController.m
+//  XWPageViewController
+//
+//  Created by Zhang Xiaowei on 15/7/20.
+//  Copyright (c) 2015年 Zhang Xiaowei. All rights reserved.
+//
+
+#import "BaseViewController.h"
+@interface BaseViewController()<UITableViewDataSource, UITableViewDelegate>
+@property (nonatomic, strong) UITableView *tableView;
+@property (nonatomic, strong) NSMutableArray *dataSource;
+@end
+
+@implementation BaseViewController
+- (void)viewDidLoad{
+    [super viewDidLoad];
+    [self tableView];
+}
+
+- (void)xwPageViewController:(XWPageViewController *)xwPageViewController showFrame:(CGRect)frame{
+    self.tableView.frame = frame;
+}
+
+#pragma mark - UITableView Datasource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 20;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"1234wsfadf"];
+    
+    cell.textLabel.text = [self.dataSource objectAtIndex:indexPath.row];
+    
+    return cell;
+}
+
+#pragma mark - UITableView Delegate
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+}
+
+- (UITableView *)tableView{
+    if (!_tableView) {
+        _tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+        _tableView.delegate = self;
+        _tableView.dataSource = self;
+        _tableView.separatorStyle = UITableViewCellSeparatorStyleSingleLine;
+        [_tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"1234wsfadf"];
+        [self.view addSubview:_tableView];
+        _tableView.rowHeight = 44;
+    }
+    return _tableView;
+}
+
+- (NSMutableArray *)dataSource{
+    if (!_dataSource) {
+        _dataSource = [NSMutableArray array];
+        for (NSInteger i = 0; i < 20; i++) {
+            [_dataSource addObject:[NSString stringWithFormat:@"123%ld",(long)i]];
+        }
+    }
+    return _dataSource;
+}
+
+
+
+@end
